e7a755b93de2fbe8d0c264c7d5b440eb8575a5b6
ravilella1234 <ravilella1234>
2019-10-04 08:31:24 +0530

    added 2nd line

--- SampleDemoproject/src/com/pack/SecondClass.java
+++ SampleDemoproject/src/com/pack/SecondClass.java
@@ -5,6 +5,7 @@ public class SecondClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("This is first Line of code");
+		System.out.println("This is Second Line of code");
 	}
 
 }
