2943ead7c9c1f011eecaf34b8c64e0db82f7dd19
ravilella1234 <37369566+ravilella1234@users.noreply.github.com>
2019-10-04 08:53:26 +0530

    Update FirstClass.java

--- SampleDemoproject/src/com/pack/FirstClass.java
+++ SampleDemoproject/src/com/pack/FirstClass.java
@@ -2,6 +2,11 @@ package com.pack;
 
 public class FirstClass {
 
+	public void add()
+	{
+	        int a,b,d;
+		d=a+b;
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("iam firstclass");
